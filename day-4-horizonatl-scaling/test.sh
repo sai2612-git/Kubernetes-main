b6b6529580f423b9ecf9793c967af8e29e3b9574
Sai Krishna <saiaws2612@gmail.com>
2025-02-25 13:12:25 +0530

    Update test.sh

--- day-4-horizonatl-scaling/test.sh
+++ day-4-horizonatl-scaling/test.sh
@@ -1,3 +1,6 @@
-for j in {1..10}; do  for i in {1..1000}; do
-    curl -s -o /dev/null -w "%{http_code}\n" http://a0fbf831ca4a94936954790ef7a89bc4-104519041.ap-south-1.elb.amazonaws.com/ &  done
-  waitdone
+for j in {1..10}; do  
+  for i in {1..1000}; do
+    curl -s -o /dev/null -w "%{http_code}\n" http://a52d688738c8f4adeb16bc92b8ee05eb-739072275.ap-south-1.elb.amazonaws.com/ &  
+  done
+  wait  # Correct command to wait for all background jobs
+done
