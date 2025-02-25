e3430ef5a6114a453aa2f2fd89446c5160b9f9a1
Sai Krishna <saiaws2612@gmail.com>
2025-02-25 13:16:21 +0530

    Update test.sh

--- day-4-horizonatl-scaling/test.sh
+++ day-4-horizonatl-scaling/test.sh
@@ -1,6 +1,6 @@
 for j in {1..10}; do  
   for i in {1..1000}; do
-    curl -s -o /dev/null -w "%{http_code}\n" http://a52d688738c8f4adeb16bc92b8ee05eb-739072275.ap-south-1.elb.amazonaws.com/ &  
+    curl -s -o /dev/null -w "%{http_code}\n" http://a52d688738c8f4adeb16bc92b8ee05eb-739072275.ap-south-1.elb.amazonaws.com/ &  # change this http address with pod loadbalancer
   done
   wait  # Correct command to wait for all background jobs
 done
